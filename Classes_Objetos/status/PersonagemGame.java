1a856e1f114e6749c09e464d5d6ca2b0905f9712
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-08-24 18:46:08 -0300

    feat(status): adicionar atributo status e lógica vivo/morto

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,49 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    // Setter de saúde já cuida do status
+    public void setSaudeAtual(int saudeAtual) {
+        if (saudeAtual < 0) {
+            this.saudeAtual = 0;
+        } else if (saudeAtual > 100) {
+            this.saudeAtual = 100;
+        } else {
+            this.saudeAtual = saudeAtual;
+        }
+
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter apenas de leitura para status
+    public String getStatus() {
+        return status;
+    }
+
+    // Métodos que usam o setter
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(this.saudeAtual - quantidadeDeDano);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(this.saudeAtual + quantidadeDeCura);
+    }
+}
