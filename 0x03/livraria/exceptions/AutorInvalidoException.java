bd77ea84f5c2ce2a68be7b6f3096fbd1ae6d64ff
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-09-21 19:07:00 -0300

    feat(livraria): implementar modelo de livros com validações e edição ouro

---
+++ 0x03/livraria/exceptions/AutorInvalidoException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class AutorInvalidoException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public AutorInvalidoException(String msg) {
+        super(msg);
+    }
+}
