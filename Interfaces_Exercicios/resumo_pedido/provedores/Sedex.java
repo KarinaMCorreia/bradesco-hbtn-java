41eb3492e71775261d7a416554db7b23e8ea8747
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-09-21 16:18:16 -0300

    feat(resumo_pedido): implementar cálculo de frete com provedores (SEDEX, JADLOG, LOGGI)

---
+++ Interfaces_Exercicios/resumo_pedido/provedores/Sedex.java
@@ -0,0 +1,15 @@
+package provedores;
+
+public class Sedex implements ProvedorFrete {
+
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+        double taxa = (peso > 1000.0) ? 0.10 : 0.05;
+        return new Frete(valor * taxa, obterTipoProvedorFrete());
+    }
+
+    @Override
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return TipoProvedorFrete.SEDEX;
+    }
+}
