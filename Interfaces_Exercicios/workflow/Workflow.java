1853d45ca6b6eed37f6fec51bcb12e4eb7ae1bfd
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-09-21 16:09:30 -0300

    feat(workflow): implementar mecanismo de workflow

---
+++ Interfaces_Exercicios/workflow/Workflow.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import atividades.Atividade;
+
+public class Workflow {
+
+    private List<Atividade> atividades = new ArrayList<>();
+
+    public void registrarAtividade(Atividade atividade) {
+        if (atividade != null) {
+            atividades.add(atividade);
+        }
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+}
