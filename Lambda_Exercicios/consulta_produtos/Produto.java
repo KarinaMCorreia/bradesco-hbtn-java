4d4147750be834041b3f2e7daedefac6c8426654
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-16 21:24:59 -0300

    feat: implementar filtro de produtos usando interface funcional e enum de tipos

--- Lambda_Exercicios/consulta_produtos/Produto.java
+++ Lambda_Exercicios/consulta_produtos/Produto.java
@@ -36,6 +36,7 @@ public class Produto {
 
     @Override
     public String toString() {
-        return nome + " " + preco + " " + peso + " " + quantidadeEmEstoque + " " + tipo;
+        return String.format("%s %f %f %d %s",
+                nome, preco, peso, quantidadeEmEstoque, tipo);
     }
 }
